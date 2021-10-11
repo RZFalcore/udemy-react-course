d5adaab632f3560c8a837f0618fa6ea870302700
Eugene Roy <box.rz.box@gmail.com>
2021-10-12 00:00:38 +0300

    added ExpenseItem

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem />
     </div>
   );
 }
