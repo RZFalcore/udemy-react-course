3cb2c0ed27c68502e1ccc6afd4ded4c3c991f148
Eugene Roy <box.rz.box@gmail.com>
2021-10-12 00:06:31 +0300

    add dummy code

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function ExpenseItem() {
-  return <h2>Expense Item.</h2>;
+    return (
+      <div>
+        <p>April 12 2020</p>
+        <div>
+          <h2>Subscription</h2>
+          <p>$ 257.00</p>
+        </div>
+      </div>
+    );
 }
 
 export default ExpenseItem;
