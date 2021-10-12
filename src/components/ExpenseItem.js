75cfc83ed491a97050254ce49b8f3e80edcf4908
Eugene Roy <box.rz.box@gmail.com>
2021-10-12 12:09:18 +0300

    added some styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,15 @@
 import React from "react";
-
+import "./ExpenseItem.css";
 function ExpenseItem() {
-    return (
-      <div>
-        <p>April 12 2020</p>
-        <div>
-          <h2>Subscription</h2>
-          <p>$ 257.00</p>
-        </div>
+  return (
+    <div className="expense-item">
+      <p>April 12 2020</p>
+      <div className="expense-item__description">
+        <h2>Subscription</h2>
+        <p className="expense-item__price">$ 257.50</p>
       </div>
-    );
+    </div>
+  );
 }
 
 export default ExpenseItem;
