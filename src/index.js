f5819d37b6259755b2c9399988ad087d3af0d376
Eugene Roy <box.rz.box@gmail.com>
2021-10-20 16:24:46 +0300

    prep for new project

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
-import App from "./App";
+import ExpensesApp from "./ExpensesApp";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<ExpensesApp />, document.getElementById("root"));
