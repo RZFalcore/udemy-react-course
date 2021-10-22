8758461c306f492186596bd468c17e39573c5df6
Eugene Roy <box.rz.box@gmail.com>
2021-10-22 13:26:55 +0300

    refac modal to use portals

--- src/components/AddUser/Modal/Modal.js
+++ src/components/AddUser/Modal/Modal.js
@@ -1,21 +1,44 @@
 import React from "react";
+import ReactDOM from "react-dom";
+
 import "./Modal.css";
+
+const BackDrop = ({ closeModal }) => {
+  return <div className="backdrop" onClick={closeModal} />;
+};
+
+const ModalOverlay = ({ title, message, closeModal }) => {
+  return (
+    <div className="modal">
+      <header className="header">
+        <h2>{title}</h2>
+      </header>
+      <div className="content">
+        <p>{message}</p>
+      </div>
+      <footer className="actions">
+        <button onClick={closeModal}>Close</button>
+      </footer>
+    </div>
+  );
+};
 
 const Modal = ({ title, message, closeModal }) => {
   return (
-    <div className="backdrop" onClick={closeModal}>
-      <div className="modal">
-        <header className="header">
-          <h2>{title}</h2>
-        </header>
-        <div className="content">
-          <p>{message}</p>
-        </div>
-        <footer className="actions">
-          <button onClick={closeModal}>Close</button>
-        </footer>
-      </div>
-    </div>
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <BackDrop closeModal={closeModal} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={title}
+          message={message}
+          closeModal={closeModal}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </React.Fragment>
   );
 };
 
