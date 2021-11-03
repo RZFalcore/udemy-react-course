36e14e631b4bf5ba0da7c7e45ecb33d10e204976
Eugene Roy <box.rz.box@gmail.com>
2021-11-03 22:20:34 +0200

    playing with render

--- src/Pages/OptimizationApp.js
+++ src/Pages/OptimizationApp.js
@@ -1,11 +1,21 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Button from "../components/OptimizationProject/UI/Button";
 import styles from "./OptimizationApp.module.css";
 
 const OptimizationApp = () => {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraphHanlder = () =>
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  console.log("App render!");
   return (
     <div className={styles.app}>
       <h1>Hello there!</h1>
+      {showParagraph && <p>New paragraph.</p>}
+      <Button onClick={toggleParagraphHanlder}>
+        {/* {showParagraph ? "Hide" : "Show"} button! */}
+        Toggle button!
+      </Button>
     </div>
   );
 };
