db8c3064bd0b12ed37002a7dca3fb04d1f9d2de1
Eugene Roy <box.rz.box@gmail.com>
2021-11-07 18:20:57 +0200

    more preps

--- src/Pages/HttpRequestsApp.js
+++ src/Pages/HttpRequestsApp.js
@@ -1,7 +1,35 @@
 import React from "react";
 
+import MoviesList from "../components/HttpRequests/MoviesList";
+import styles from "./HttpRequestApp.module.css";
+
+const dummyMovies = [
+  {
+    id: 1,
+    title: "Some Dummy Movie",
+    openingText: "This is the opening text of the movie",
+    releaseDate: "2021-05-18",
+  },
+  {
+    id: 2,
+    title: "Some Dummy Movie 2",
+    openingText: "This is the second opening text of the movie",
+    releaseDate: "2021-05-19",
+  },
+];
+
 const HttpRequestsApp = () => {
-  return <h1>App</h1>;
+  return (
+    <React.Fragment>
+      <section className={styles.section}>
+        <button className={styles.button}>Fetch Movies</button>
+      </section>
+      <section className={styles.section}>
+        <MoviesList movies={dummyMovies} />
+      </section>
+    </React.Fragment>
+  );
 };
+  
 
 export default HttpRequestsApp;
