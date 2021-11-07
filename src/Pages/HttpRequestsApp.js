4744e406041cf7211c94e46ab7faf858762ea023
Eugene Roy <box.rz.box@gmail.com>
2021-11-07 23:44:59 +0200

    connect firebase db, impl new films adding

--- src/Pages/HttpRequestsApp.js
+++ src/Pages/HttpRequestsApp.js
@@ -13,14 +13,16 @@ const HttpRequestsApp = () => {
       setIsLoading(true);
       setError(null);
 
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch(
+        "https://ud-react-http-default-rtdb.europe-west1.firebasedatabase.app/movies.json"
+      );
       const data = await response.json();
-      const transformedData = data.results.map((data) => {
+      const transformedData = Object.entries(data).map(([id, value]) => {
         return {
-          id: data.episode_id,
-          title: data.title,
-          openingText: data.opening_crawl,
-          realeseDate: data.realese_date,
+          id: id,
+          title: value.title,
+          openingText: value.openingText,
+          releaseDate: value.releaseDate,
         };
       });
 
@@ -33,7 +35,26 @@ const HttpRequestsApp = () => {
     }
   }, []);
 
-  const addMovieHandler = (movie) => console.log(movie);
+  const addMovieHandler = async (movie) => {
+    console.log(movie);
+    try {
+      const response = await fetch(
+        "https://ud-react-http-default-rtdb.europe-west1.firebasedatabase.app/movies.json",
+        {
+          method: "POST",
+          body: JSON.stringify(movie),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+      setError(e.message);
+    }
+  };
 
   useEffect(() => fetchMoviesHandler(), [fetchMoviesHandler]);
 
