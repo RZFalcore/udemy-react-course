eacc0f2170e85e6511ec8929d469f23a08fd4a2b
Eugene Roy <box.rz.box@gmail.com>
2021-11-07 22:54:28 +0200

    fetching req on mount, handling side effects via useEffect and useCallback

--- src/Pages/HttpRequestsApp.js
+++ src/Pages/HttpRequestsApp.js
@@ -1,60 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import MoviesList from "../components/HttpRequests/MoviesList";
 import styles from "./HttpRequestApp.module.css";
 
 const HttpRequestsApp = () => {
-    const [movies, setMovies] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    const fetchMoviesHandler = async () => {
-      try {
-        setIsLoading(true);
-        setError(null);
+  const fetchMoviesHandler = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      setError(null);
 
-        const response = await fetch("https://swapi.dev/api/films/");
-        const data = await response.json();
-        const transformedData = data.results.map((data) => {
-          return {
-            id: data.episode_id,
-            title: data.title,
-            openingText: data.opening_crawl,
-            realeseDate: data.realese_date,
-          };
-        });
+      const response = await fetch("https://swapi.dev/api/films/");
+      const data = await response.json();
+      const transformedData = data.results.map((data) => {
+        return {
+          id: data.episode_id,
+          title: data.title,
+          openingText: data.opening_crawl,
+          realeseDate: data.realese_date,
+        };
+      });
 
-        setMovies(transformedData);
-        setIsLoading(false);
-      } catch (e) {
-        setIsLoading(false);
-        setError(e.message);
-        console.log(e);
-      }
-    };
+      setMovies(transformedData);
+      setIsLoading(false);
+    } catch (e) {
+      setIsLoading(false);
+      setError(e.message);
+      console.log(e);
+    }
+  }, []);
 
-    let output;
-    if (!isLoading && movies.length > 0)
-      output = <MoviesList movies={movies} />;
-    if (!isLoading && movies.length === 0) output = <p>No movies founded.</p>;
-    if (isLoading) output = <p>Loading...</p>;
-    if (error)
-      output = (
-        <>
-          <h2>Some error occures...</h2>
-          <p>{error}</p>
-        </>
-      );
+  useEffect(() => fetchMoviesHandler(), [fetchMoviesHandler]);
 
-    return (
-      <React.Fragment>
-        <section className={styles.section}>
-          <button className={styles.button} onClick={fetchMoviesHandler}>
-            Fetch Movies
-          </button>
-        </section>
-        <section className={styles.section}>{output}</section>
-      </React.Fragment>
+  let output;
+  if (!isLoading && movies.length > 0) output = <MoviesList movies={movies} />;
+  if (!isLoading && movies.length === 0) output = <p>No movies founded.</p>;
+  if (isLoading) output = <p>Loading...</p>;
+  if (error)
+    output = (
+      <>
+        <h2>Some error occures...</h2>
+        <p>{error}</p>
+      </>
     );
+
+  return (
+    <React.Fragment>
+      <section className={styles.section}>
+        <button className={styles.button} onClick={fetchMoviesHandler}>
+          Fetch Movies
+        </button>
+      </section>
+      <section className={styles.section}>{output}</section>
+    </React.Fragment>
+  );
 };
 
 export default HttpRequestsApp;
