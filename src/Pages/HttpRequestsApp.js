e700e44145dd2fcd770dc0acb261e372ad9d473a
Eugene Roy <box.rz.box@gmail.com>
2021-11-07 22:39:33 +0200

    added error handling

--- src/Pages/HttpRequestsApp.js
+++ src/Pages/HttpRequestsApp.js
@@ -3,44 +3,58 @@ import MoviesList from "../components/HttpRequests/MoviesList";
 import styles from "./HttpRequestApp.module.css";
 
 const HttpRequestsApp = () => {
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-  const fetchMoviesHandler = async () => {
-    try {
-      setIsLoading(true);
-      const response = await fetch("https://swapi.dev/api/films/");
-      const data = await response.json();
-      const transformedData = data.results.map((data) => {
-        return {
-          id: data.episode_id,
-          title: data.title,
-          openingText: data.opening_crawl,
-          realeseDate: data.realese_date,
-        };
-      });
-      setMovies(transformedData);
-      setIsLoading(false);
-    } catch (e) {
-      setIsLoading(false);
-      console.log(e);
-    }
-  };
+    const fetchMoviesHandler = async () => {
+      try {
+        setIsLoading(true);
+        setError(null);
 
-  return (
-    <React.Fragment>
-      <section className={styles.section}>
-        <button className={styles.button} onClick={fetchMoviesHandler}>
-          Fetch Movies
-        </button>
-      </section>
-      <section className={styles.section}>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>No movies founded.</p>}
-        {isLoading && <p>Loading...</p>}
-      </section>
-    </React.Fragment>
-  );
+        const response = await fetch("https://swapi.dev/api/films/");
+        const data = await response.json();
+        const transformedData = data.results.map((data) => {
+          return {
+            id: data.episode_id,
+            title: data.title,
+            openingText: data.opening_crawl,
+            realeseDate: data.realese_date,
+          };
+        });
+
+        setMovies(transformedData);
+        setIsLoading(false);
+      } catch (e) {
+        setIsLoading(false);
+        setError(e.message);
+        console.log(e);
+      }
+    };
+
+    let output;
+    if (!isLoading && movies.length > 0)
+      output = <MoviesList movies={movies} />;
+    if (!isLoading && movies.length === 0) output = <p>No movies founded.</p>;
+    if (isLoading) output = <p>Loading...</p>;
+    if (error)
+      output = (
+        <>
+          <h2>Some error occures...</h2>
+          <p>{error}</p>
+        </>
+      );
+
+    return (
+      <React.Fragment>
+        <section className={styles.section}>
+          <button className={styles.button} onClick={fetchMoviesHandler}>
+            Fetch Movies
+          </button>
+        </section>
+        <section className={styles.section}>{output}</section>
+      </React.Fragment>
+    );
 };
 
 export default HttpRequestsApp;
