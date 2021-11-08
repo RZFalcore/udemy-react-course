3f4c1a1902501dfe32403d5160da0509b9941e59
Eugene Roy <box.rz.box@gmail.com>
2021-11-08 16:11:23 +0200

    added tasks comps, useFetchTasks in progress

--- src/Pages/CustomHooksApp.js
+++ src/Pages/CustomHooksApp.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DecCounter from "../components/CustomHooks/DecCounter";
 import IncCounter from "../components/CustomHooks/IncCounter";
+import Tasks from "../components/CustomHooks/Tasks/Tasks";
+import NewTask from "../components/CustomHooks/NewTask/NewTask";
 
 const CustomHooksApp = () => {
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
   return (
     <>
       <IncCounter />
       <DecCounter />
+      <React.Fragment>
+        <NewTask onAddTask={taskAddHandler} />
+        <Tasks
+          items={tasks}
+          loading={isLoading}
+          error={error}
+          onFetch={fetchTasks}
+        />
+      </React.Fragment>
     </>
   );
 };
---
+++ src/components/CustomHooks/hooks/useFetchTasks.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+
+const useFetchTasks = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [tasks, setTasks] = useState([]);
+
+  const fetchTasks = async (taskText) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        "https://ud-react-http-default-rtdb.europe-west1.firebasedatabase.app/tasks.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+
+      const loadedTasks = [];
+
+      for (const taskKey in data) {
+        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+};
+
+export default useFetchTasks;
