a1325d71378e89b9956a9817299f7ef303ba20f6
Eugene Roy <box.rz.box@gmail.com>
2021-11-10 18:06:49 +0200

    basic input validation

--- src/components/Forms/SimpleInput.js
+++ src/components/Forms/SimpleInput.js
@@ -2,26 +2,34 @@ import React, { useState, useRef } from "react";
 
 const SimpleInput = (props) => {
   const [inputValue, setInputValue] = useState("");
+  const [inputValueIsValid, setInputValueIsValid] = useState(true);
+
   const inputRef = useRef();
 
   const changeInputHandler = (e) => {
+    if (!inputValueIsValid ) setInputValueIsValid(true)
     setInputValue(e.target.value);
   };
 
   const formSubmitHandler = (e) => {
     e.preventDefault();
+    if (inputValue.trim().length === 0) {
+      setInputValueIsValid(false)
+    }
     console.log(inputValue);
 
-    const refInputValue = inputRef.current.value;
-    console.log(refInputValue);
+    // const refInputValue = inputRef.current.value;
+    // console.log(refInputValue);
 
     // refInputValue.current.value = "";  --- POSSIBLE, BUT NOT RECOMENDED
     setInputValue("");
   };
 
+  const formStyles = inputValueIsValid ? "form-control" : "form-control invalid"
+
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={formStyles}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={inputRef}
@@ -30,12 +38,13 @@ const SimpleInput = (props) => {
           onChange={changeInputHandler}
           value={inputValue}
         />
+        {!inputValueIsValid && <p className="error-text">Name must not be empty</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
       </div>
     </form>
   );
-};
+};;
 
 export default SimpleInput;
