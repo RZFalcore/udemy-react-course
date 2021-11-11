8de2902e3290479455cf044aa27c9776279de5db
Eugene Roy <box.rz.box@gmail.com>
2021-11-11 17:24:32 +0200

    refac useInput2 to use reducer

--- src/components/Forms/hooks/useInput2.js
+++ src/components/Forms/hooks/useInput2.js
@@ -1,27 +1,46 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
+
+const defaultState = { value: "", isTouched: false };
+
+const inputReducer = (state, { type, payload }) => {
+  switch (type) {
+    case "CHANGE":
+      return { value: payload, isTouched: state.isTouched };
+    case "BLUR":
+      return { value: state.value, isTouched: true };
+    case "RESET":
+      return { value: "", isTouched: false };
+    default:
+      return state;
+  }
+};
 
 const useInput = (validator) => {
-  const [value, setValue] = useState("");
-  const [inputWasTouched, setInputWasTouched] = useState(false);
+  const [inputState, dispatch] = useReducer(inputReducer, defaultState);
+  //   const [value, setValue] = useState("");
+  //   const [inputWasTouched, setInputWasTouched] = useState(false);
 
   const changeValueHandler = (e) => {
-    setValue(e.target.value);
+    // setValue(e.target.value);
+    dispatch({ type: "CHANGE", payload: e.target.value });
   };
 
   const inputOnBlurHandler = (e) => {
-    setInputWasTouched(true);
+    //   setInputWasTouched(true);
+    dispatch({ type: "BLUR" });
   };
 
   const reset = () => {
-    setValue("");
-    setInputWasTouched(false);
+    // setValue("");
+    // setInputWasTouched(false);
+    dispatch({ type: "RESET" });
   };
 
-  const isValid = validator(value);
-  const hasError = !isValid && inputWasTouched;
+  const isValid = validator(inputState.value);
+  const hasError = !isValid && inputState.isTouched;
 
   return {
-    value,
+    value: inputState.value,
     isValid,
     hasError,
     changeValueHandler,
