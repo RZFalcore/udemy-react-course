180bc9fb2270dff402de317a8ee852080e3b096b
Eugene Roy <box.rz.box@gmail.com>
2021-11-14 21:55:50 +0200

    add refs in Checkout

--- src/components/FoodOrderProject/Cart/Checkout.js
+++ src/components/FoodOrderProject/Cart/Checkout.js
@@ -1,29 +1,38 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./Checkout.module.css";
 
 const Checkout = ({ onCloseModal }) => {
+  const nameRef = useRef();
+  const streetRef = useRef();
+  const postalRef = useRef();
+  const cityRef = useRef();
 
   const formSubmitHandler = (e) => {
     e.preventDefault();
+
+    const nameValue = nameRef.current.value;
+    const streetValue = streetRef.current.value;
+    const postalValue = postalRef.current.value;
+    const cityValue = cityRef.current.value;
   };
 
   return (
     <form className={styles.form} onSubmit={formSubmitHandler}>
       <div className={styles.control}>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" ref={nameRef} />
       </div>
       <div className={styles.control}>
         <label htmlFor="street">Street</label>
-        <input type="text" id="street" />
+        <input type="text" id="street" ref={streetRef} />
       </div>
       <div className={styles.control}>
         <label htmlFor="postal">Postal Code</label>
-        <input type="text" id="postal" />
+        <input type="text" id="postal" ref={postalRef} />
       </div>
       <div className={styles.control}>
         <label htmlFor="city">City</label>
-        <input type="text" id="city" />
+        <input type="text" id="city" ref={cityRef} />
       </div>
       <div className={styles.actions}>
         <button type="button" onClick={onCloseModal}>
