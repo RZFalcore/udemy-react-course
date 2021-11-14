35cde4acc1d8573e52d68a2709c0e3ff3b5d1cef
Eugene Roy <box.rz.box@gmail.com>
2021-11-14 23:16:01 +0200

    delete refs in checkout, used useInput hook on name input

--- src/components/FoodOrderProject/Cart/Checkout.js
+++ src/components/FoodOrderProject/Cart/Checkout.js
@@ -1,44 +1,68 @@
-import React, { useRef } from "react";
+import React from "react";
+import useInput from "../hooks/useInput";
 import styles from "./Checkout.module.css";
 
 const Checkout = ({ onCloseModal }) => {
-  const nameRef = useRef();
-  const streetRef = useRef();
-  const postalRef = useRef();
-  const cityRef = useRef();
+  const isNotEmpty = (value) => value.trim().length > 0;
+
+  const {
+    value: name,
+    isValid: nameIsValid,
+    hasError: nameHasError,
+    changeValueHandler: changeNameValue,
+    onBlurHandler: nameBlur,
+    reset: nameReset,
+  } = useInput(isNotEmpty);
 
   const formSubmitHandler = (e) => {
     e.preventDefault();
 
-    const nameValue = nameRef.current.value;
-    const streetValue = streetRef.current.value;
-    const postalValue = postalRef.current.value;
-    const cityValue = cityRef.current.value;
+    const formIsValid = nameIsValid;
+    console.log(formIsValid);
+    if (!formIsValid) {
+      return;
+    }
+    console.log(name);
+
+    nameReset();
   };
+
+  const nameStyles = nameHasError
+    ? `${styles.control} ${styles.invalid}`
+    : `${styles.control}`;
 
   return (
     <form className={styles.form} onSubmit={formSubmitHandler}>
-      <div className={styles.control}>
+      <div className={nameStyles}>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" ref={nameRef} />
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={changeNameValue}
+          onBlur={nameBlur}
+        />
+        {nameHasError && <p>Please enter valid name!</p>}
       </div>
       <div className={styles.control}>
         <label htmlFor="street">Street</label>
-        <input type="text" id="street" ref={streetRef} />
+        <input type="text" id="street" />
       </div>
       <div className={styles.control}>
         <label htmlFor="postal">Postal Code</label>
-        <input type="text" id="postal" ref={postalRef} />
+        <input type="text" id="postal" />
       </div>
       <div className={styles.control}>
         <label htmlFor="city">City</label>
-        <input type="text" id="city" ref={cityRef} />
+        <input type="text" id="city" />
       </div>
       <div className={styles.actions}>
         <button type="button" onClick={onCloseModal}>
           Cancel
         </button>
-        <button className={styles.submit}>Confirm</button>
+        <button type="submit" className={styles.submit}>
+          Confirm
+        </button>
       </div>
     </form>
   );
