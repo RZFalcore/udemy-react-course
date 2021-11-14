ebcc3c8104a86f78c2d4d3ec7797a0714b597ccd
Eugene Roy <box.rz.box@gmail.com>
2021-11-14 21:51:12 +0200

    update some styles, added new fields in Checkout

--- src/components/FoodOrderProject/Cart/Checkout.js
+++ src/components/FoodOrderProject/Cart/Checkout.js
@@ -2,20 +2,35 @@ import React from "react";
 import styles from "./Checkout.module.css";
 
 const Checkout = ({ onCloseModal }) => {
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <form>
+    <form className={styles.form} onSubmit={formSubmitHandler}>
       <div className={styles.control}>
-        <label htmlFor="name">Your name</label>
+        <label htmlFor="name">Your Name</label>
         <input type="text" id="name" />
       </div>
       <div className={styles.control}>
-        <label htmlFor="adress">Your adress</label>
-        <input type="text" id="adress" />
+        <label htmlFor="street">Street</label>
+        <input type="text" id="street" />
       </div>
-      <button type="button" onClick={onCloseModal}>
-        Cancel
-      </button>
-      <button type="submit">Confirm</button>
+      <div className={styles.control}>
+        <label htmlFor="postal">Postal Code</label>
+        <input type="text" id="postal" />
+      </div>
+      <div className={styles.control}>
+        <label htmlFor="city">City</label>
+        <input type="text" id="city" />
+      </div>
+      <div className={styles.actions}>
+        <button type="button" onClick={onCloseModal}>
+          Cancel
+        </button>
+        <button className={styles.submit}>Confirm</button>
+      </div>
     </form>
   );
 };
