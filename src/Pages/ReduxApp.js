29b901c0ac6aa0ea7776a166d35e21f0662475df
Eugene Roy <box.rz.box@gmail.com>
2021-11-16 14:01:58 +0200

    connecting store to ReduxApp

--- src/Pages/ReduxApp.js
+++ src/Pages/ReduxApp.js
@@ -1,7 +1,13 @@
 import Counter from "../components/Redux/Counter";
+import { Provider } from "react-redux";
+import store from "../components/Redux/store/index";
 
 function App() {
-  return <Counter />;
+  return (
+    <Provider store={store}>
+      <Counter />
+    </Provider>
+  );
 }
 
 export default App;
