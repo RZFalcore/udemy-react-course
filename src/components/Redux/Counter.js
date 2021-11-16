ab5d2e746687b6d87b90aa77613d9f60d3306b7c
Eugene Roy <box.rz.box@gmail.com>
2021-11-16 15:22:35 +0200

    connected Counter to store, handling value changes

--- src/components/Redux/Counter.js
+++ src/components/Redux/Counter.js
@@ -1,12 +1,21 @@
-import classes from './Counter.module.css';
+import { useSelector, useDispatch } from "react-redux";
+import styles from "./Counter.module.css";
 
 const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const handleInc = () => dispatch({ type: "INCREMENT" });
+  const handleDec = () => dispatch({ type: "DECREMENT" });
+
   const toggleCounterHandler = () => {};
 
   return (
-    <main className={classes.counter}>
+    <main className={styles.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={styles.value}>{counter}</div>
+      <button onClick={handleInc}>Increment</button>
+      <button onClick={handleDec}>Decrement</button>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
