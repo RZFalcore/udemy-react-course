839dfd1f1516a07b69e7402f138c318aba938e74
Eugene Roy <box.rz.box@gmail.com>
2021-11-16 13:38:08 +0200

    more basics

--- src/components/Redux/reduxDemo.js
+++ src/components/Redux/reduxDemo.js
@@ -3,9 +3,18 @@ const redux = require("redux");
 const defaultStore = { counter: 0 };
 
 const incReducer = (store = defaultStore, action) => {
-  return {
-    counter: store.counter + 1,
-  };
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        counter: store.counter + 1,
+      };
+
+    case "DECREMENT":
+      return { counter: store.counter - 1 };
+
+    default:
+      return store;
+  }
 };
 
 const store = redux.createStore(incReducer);
@@ -16,4 +25,7 @@ const counterSubscriber = () => {
 };
 
 store.subscribe(counterSubscriber);
+
 store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "DECREMENT" });
