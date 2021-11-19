75dd0e11593dd6350fa374bb2448dd5e6cacf6ba
Eugene Roy <box.rz.box@gmail.com>
2021-11-19 12:25:01 +0200

    changed dummy values to store values in Cart

--- src/components/ReduxCart/Cart/Cart.js
+++ src/components/ReduxCart/Cart/Cart.js
@@ -1,15 +1,17 @@
+import { useSelector } from "react-redux";
 import Card from "../UI/Card";
 import CartItem from "./CartItem";
 import styles from "./Cart.module.css";
 
 const Cart = (props) => {
+  const cartItems = useSelector((state) => state.cartItems);
   return (
     <Card className={styles.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
-          item={{ title: "Test Item", quantity: 3, total: 18, price: 6 }}
-        />
+        {cartItems.map(({ id, title, quantity, total, price }) => (
+          <CartItem key={id} item={{ title, quantity, total, price }} />
+        ))}
       </ul>
     </Card>
   );
