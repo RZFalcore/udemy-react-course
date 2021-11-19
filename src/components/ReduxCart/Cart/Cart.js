f91dd52a9416fe2c6741dae0350c415e854bd746
Eugene Roy <box.rz.box@gmail.com>
2021-11-19 13:11:59 +0200

    conditional render of cart items

--- src/components/ReduxCart/Cart/Cart.js
+++ src/components/ReduxCart/Cart/Cart.js
@@ -3,17 +3,22 @@ import Card from "../UI/Card";
 import CartItem from "./CartItem";
 import styles from "./Cart.module.css";
 
-const Cart = (props) => {
+const Cart = () => {
   const cartItems = useSelector((state) => state.cartItems);
-  console.log("cartItems", cartItems);
+
+  const cartList = cartItems.map(({ id, title, quantity, total, price }) => (
+    <CartItem key={id} item={{ id, title, quantity, total, price }} />
+  ));
   return (
     <Card className={styles.cart}>
-      <h2>Your Shopping Cart</h2>
-      <ul>
-        {cartItems.map(({ id, title, quantity, total, price }) => (
-          <CartItem key={id} item={{ id, title, quantity, total, price }} />
-        ))}
-      </ul>
+      {cartItems.length ? (
+        <>
+          <h2>Your Shopping Cart</h2>
+          <ul>{cartList}</ul>
+        </>
+      ) : (
+        <h2>Your cart is empty!</h2>
+      )}
     </Card>
   );
 };
