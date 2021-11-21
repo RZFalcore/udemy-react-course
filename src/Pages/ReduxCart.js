179f2b256e7c42590bb932de66388440bc85acf9
Eugene Roy <box.rz.box@gmail.com>
2021-11-21 16:55:22 +0200

    handling cart data fetching

--- src/Pages/ReduxCart.js
+++ src/Pages/ReduxCart.js
@@ -5,10 +5,14 @@ import { cartActions } from "../components/ReduxCart/store/cartReducer";
 import Cart from "../components/ReduxCart/Cart/Cart";
 import Layout from "../components/ReduxCart/Layout/Layout";
 import Products from "../components/ReduxCart/Shop/Products";
+import Notification from "../components/ReduxCart/UI/Notification";
+
+let isIntial = true;
 
 function ReduxApp() {
   const cart = useSelector((state) => state.cart.cart);
   const showCart = useSelector((state) => state.cart.showCart);
+  const notification = useSelector((state) => state.cart.notification);
 
   const dispatch = useDispatch();
 
@@ -21,21 +25,13 @@ function ReduxApp() {
           message: "Fetching cart data.",
         })
       );
-      const response = fetch(
+      const response = await fetch(
         "https://ud-react-http-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
         { method: "PUT", body: JSON.stringify(cart) }
       );
 
-      if (!response.ok)
-        dispatch(
-          cartActions.setNotification({
-            status: "error",
-            title: "Errror",
-            message: "Send cart data error",
-          })
-        );
-
-      // const responseData = await response.json();
+      console.log(response);
+      if (!response.ok) throw new Error("Send cart data error");
 
       dispatch(
         cartActions.setNotification({
@@ -45,13 +41,26 @@ function ReduxApp() {
         })
       );
     };
+
+    sendCartData().catch((error) => {
+      dispatch(
+        cartActions.setNotification({
+          status: "error",
+          title: "Error",
+          message: error.message,
+        })
+      );
+    });
   }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && <Notification {...notification} />}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
