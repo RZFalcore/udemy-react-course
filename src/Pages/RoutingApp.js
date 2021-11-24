443d68cca5de01d14548029397ad7181c431b88c
Eugene Roy <box.rz.box@gmail.com>
2021-11-24 16:09:55 +0200

    basic routing

--- src/Pages/RoutingApp.js
+++ src/Pages/RoutingApp.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ProductsPage from "../components/RoutingApp/Pages/ProductsPage";
+import WelcomePage from "../components/RoutingApp/Pages/WelcomePage";
+
 import "./RoutingApp.css";
 
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Router>
+      <div>
+        <h2>Let's get started!</h2>
+      </div>
+      <Route path="/welcome">
+        <WelcomePage />
+      </Route>
+      <Route path="/products">
+        <ProductsPage />
+      </Route>
+    </Router>
   );
 }
 
