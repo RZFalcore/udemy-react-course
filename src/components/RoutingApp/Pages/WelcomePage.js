d5578e60b6d72414e3f0ae13f3996f16db7a2f9b
Eugene Roy <box.rz.box@gmail.com>
2021-11-24 21:47:51 +0200

    nested routes

--- src/components/RoutingApp/Pages/WelcomePage.js
+++ src/components/RoutingApp/Pages/WelcomePage.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { Route } from "react-router-dom";
 
-const WelcomePage = () => <h1>Welcome Page</h1>;
+const WelcomePage = () => (
+  <section>
+    <h1>Welcome Page</h1>
+    <Route path="/welcome/new-user">
+      <h2>Hello there, userName!</h2>
+    </Route>
+  </section>
+);
 
 export default WelcomePage;
