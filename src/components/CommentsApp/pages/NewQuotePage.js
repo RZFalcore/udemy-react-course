d3b394b6e3c5e0d2c76cfb78007dbf89a500a86b
Eugene Roy <box.rz.box@gmail.com>
2021-11-30 22:32:24 +0200

    implementing programmatic navigation

--- src/components/CommentsApp/pages/NewQuotePage.js
+++ src/components/CommentsApp/pages/NewQuotePage.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { useHistory } from "react-router";
 import QuoteForm from "../quotes/QuoteForm";
 
 const NewQuotePage = () => {
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+    history.push("/quotes");
   };
   return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
