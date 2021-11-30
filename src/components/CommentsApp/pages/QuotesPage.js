358a700feaf0038d6255258713618269df49759e
Eugene Roy <box.rz.box@gmail.com>
2021-11-30 19:03:32 +0200

    connected quotes list

--- src/components/CommentsApp/pages/QuotesPage.js
+++ src/components/CommentsApp/pages/QuotesPage.js
@@ -1,8 +1,14 @@
 import React from "react";
 import QuoteList from "../quotes/QuoteList";
 
+const DUMMY_DATA = [
+  { id: "1", author: "Harry", text: "Hello there!" },
+  { id: "2", author: "Endy", text: "General Kenoby!" },
+];
+
 const QuotesPage = () => {
-  return <QuoteList />;
+  return <QuoteList quotes={DUMMY_DATA} />;
+  // return <h1>All Quotes page</h1>;
 };
 
 export default QuotesPage;
