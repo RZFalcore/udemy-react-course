a0f3df6156c2db48bcf870ebd3bab110bdf83a7c
Eugene Roy <box.rz.box@gmail.com>
2021-12-01 13:28:45 +0200

    working with querry params, added quotes sorting

--- src/components/CommentsApp/quotes/QuoteList.js
+++ src/components/CommentsApp/quotes/QuoteList.js
@@ -1,12 +1,40 @@
 import React from "react";
+import { useHistory, useLocation } from "react-router";
 import QuoteItem from "./QuoteItem";
 import styles from "./QuoteList.module.css";
 
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
+
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search);
+  const isSortingAscending = queryParams.get("sort") === "asc";
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
+  const changeSortingHandler = () => {
+    history.push("/quotes?sort=" + (isSortingAscending ? "des" : "asc"));
+  };
+
   return (
     <>
+      <div className={styles.sorting}>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortingAscending ? "descending" : "ascending"}
+        </button>
+      </div>
       <ul className={styles.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
