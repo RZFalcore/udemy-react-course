f0ee5de8e8b40643370be438af1c224780ad107b
Eugene Roy <box.rz.box@gmail.com>
2021-12-06 14:22:59 +0200

    add loading comments logic in Comments

--- src/components/CommentsApp/comments/Comments.js
+++ src/components/CommentsApp/comments/Comments.js
@@ -1,24 +1,59 @@
 import { useState } from 'react';
+import { useParams } from "react-router";
+import useHttp from "../hooks/use-http";
+import { getAllComments } from "../lib/api";
 
-import classes from './Comments.module.css';
-import NewCommentForm from './NewCommentForm';
+import NewCommentForm from "./NewCommentForm";
+import CommentsList from "./CommentsList";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import styles from "./Comments.module.css";
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
+  const { sendRequest, status, data: loadedComments } = useHttp(getAllComments);
+  const { quoteId } = useParams();
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
-  
+
+  const addedCommentHandler = () => {};
+
+  let comments;
+  if (status === "pending")
+    comments = (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+
+  if (status === "completed" && loadedComments && loadedComments.length > 0)
+    comments = <CommentsList comments={loadedComments} />;
+
+  if (
+    status === "completed" &&
+    (!loadedComments || loadedComments.length === 0)
+  )
+    <p className="centered">No comments...</p>;
+
   return (
-    <section className={classes.comments}>
+    <section className={styles.comments}>
       <h2>User Comments</h2>
       {!isAddingComment && (
-        <button className='btn' onClick={startAddCommentHandler}>
+        <button className="btn" onClick={startAddCommentHandler}>
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
+      {isAddingComment && (
+        <NewCommentForm
+          onAddingComment={addedCommentHandler}
+          quoteId={quoteId}
+        />
+      )}
       <p>Comments...</p>
     </section>
   );
