9b00ced39223b2ebafe621788d1998cfb6dcb971
Eugene Roy <box.rz.box@gmail.com>
2021-12-06 14:09:50 +0200

    added comment adding logic to newComment

--- src/components/CommentsApp/comments/NewCommentForm.js
+++ src/components/CommentsApp/comments/NewCommentForm.js
@@ -1,26 +1,39 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from "react";
+import useHttp from "../hooks/use-http";
+import { addComment } from "../lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
 
-import classes from './NewCommentForm.module.css';
+import styles from "./NewCommentForm.module.css";
 
-const NewCommentForm = (props) => {
+const NewCommentForm = ({ onAddingComment, quoteId }) => {
   const commentTextRef = useRef();
+  const { sendRequest, status, error } = useHttp(addComment);
+
+  useEffect(() => {
+    if (status === "completed" && !error) onAddingComment();
+  }, [status, error]);
 
   const submitFormHandler = (event) => {
     event.preventDefault();
 
-    // optional: Could validate here
+    const text = commentTextRef.current.value;
 
-    // send comment to server
+    sendRequest({ text }, quoteId);
   };
 
   return (
-    <form className={classes.form} onSubmit={submitFormHandler}>
-      <div className={classes.control} onSubmit={submitFormHandler}>
-        <label htmlFor='comment'>Your Comment</label>
-        <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
+    <form className={styles.form} onSubmit={submitFormHandler}>
+      {status === "pending" && (
+        <div className="centered">
+          <LoadingSpinner />
+        </div>
+      )}
+      <div className={styles.control} onSubmit={submitFormHandler}>
+        <label htmlFor="comment">Your Comment</label>
+        <textarea id="comment" rows="5" ref={commentTextRef}></textarea>
       </div>
-      <div className={classes.actions}>
-        <button className='btn'>Add Comment</button>
+      <div className={styles.actions}>
+        <button className="btn">Add Comment</button>
       </div>
     </form>
   );
