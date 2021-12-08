e61f03bfde2744c5518f059ff159547e6c068861
Eugene Roy <box.rz.box@gmail.com>
2021-12-08 18:50:29 +0200

    useNavigate

--- src/components/RoutingApp/Pages/ProductsPage.js
+++ src/components/RoutingApp/Pages/ProductsPage.js
@@ -1,21 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+// import { Link, useNavigate } from "react-router-dom";
 
-const ProductsPage = () => (
-  <section>
-    <h1>Products Page</h1>
-    <ul>
-      <li>
-        <Link to="/products/1">Product 1</Link>
-      </li>
-      <li>
-        <Link to="/products/2">Product 2</Link>
-      </li>
-      <li>
-        <Link to="/products/3">Product 3</Link>
-      </li>
-    </ul>
-  </section>
-);
+const ProductsPage = () => {
+  // const navigate = useNavigate();
+  // navigate("/products/")
+  // navigate(-1)
+  return (
+    <section>
+      <h1>Products Page</h1>
+      <ul>
+        <li>
+          <Link to="/products/1">Product 1</Link>
+        </li>
+        <li>
+          <Link to="/products/2">Product 2</Link>
+        </li>
+        <li>
+          <Link to="/products/3">Product 3</Link>
+        </li>
+      </ul>
+    </section>
+  );
+};
 
 export default ProductsPage;
