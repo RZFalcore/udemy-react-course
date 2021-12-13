a79f49fefff2f88c030feec12dc93b0b3b9cc2b2
Eugene Roy <box.rz.box@gmail.com>
2021-12-13 20:36:42 +0200

    adding registr logic

--- src/components/AuthProject/Auth/AuthForm.js
+++ src/components/AuthProject/Auth/AuthForm.js
@@ -1,25 +1,53 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 import styles from "./AuthForm.module.css";
 
 const AuthForm = () => {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
   const [isLogin, setIsLogin] = useState(true);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    const email = emailInputRef.current.value;
+    const password = passwordInputRef.current.value;
+
+    if (!isLogin) {
+      fetch(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_API_KEY}`,
+        {
+          method: "POST",
+          body: JSON.stringify({ email, password, returnSecureToken: true }),
+          // headers: { ContentType: "application/json" },
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+        .catch((e) => console.log(e));
+    }
+  };
+
   return (
     <section className={styles.auth}>
       <h1>{isLogin ? "Login" : "Sign Up"}</h1>
-      <form>
+      <form onSubmit={formSubmitHandler}>
         <div className={styles.control}>
           <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" required />
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
         <div className={styles.control}>
           <label htmlFor="password">Your Password</label>
-          <input type="password" id="password" required />
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
         </div>
         <div className={styles.actions}>
           <button>{isLogin ? "Login" : "Create Account"}</button>
