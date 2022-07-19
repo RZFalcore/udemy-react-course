5adedc26e787088445bd5ca666506500295b3695
Eugene Roy <box.rz.box@gmail.com>
2022-07-19 21:05:46 +0300

    added more test cases

--- src/components/TestingApp/Greeting.js
+++ src/components/TestingApp/Greeting.js
@@ -1,7 +1,17 @@
+import React, { useState } from "react";
+
 const Greeting = () => {
+  const [changedText, setChangedText] = useState(false);
+
+  const handleTextChange = () => {
+    setChangedText((flag) => !flag);
+  };
   return (
     <div>
       <h2>Hello there!</h2>
+      {changedText && <p>Changed!</p>}
+      {!changedText && <p>Not changed!</p>}
+      <button onClick={handleTextChange}>Change!</button>
     </div>
   );
 };
--- src/components/TestingApp/Greeting.test.js
+++ src/components/TestingApp/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom/extend-expect";
 
 import Greeting from "./Greeting";
@@ -14,5 +15,33 @@ describe("Greeting component", () => {
     // Assert
     const helloElement = screen.getByText("Hello there!", { exact: true });
     expect(helloElement).toBeInTheDocument();
+  });
+
+  test("render Not Changed text if btn not clicked", () => {
+    render(<Greeting />);
+
+    const notChangedElem = screen.getByText("Not changed!");
+    expect(notChangedElem).toBeInTheDocument();
+  });
+
+  test("render Changed if btn WAS clicked", () => {
+    // Arrange
+    render(<Greeting />);
+    // Act
+    const btnElem = screen.getByRole("button");
+    userEvent.click(btnElem);
+    // Assert
+    const changedElem = screen.getByText("Changed!");
+    expect(changedElem).toBeInTheDocument();
+  });
+
+  test("'Not changed' is not rendered while 'Change' clicked", () => {
+    render(<Greeting />);
+
+    const btnElem = screen.getByRole("button");
+    userEvent.click(btnElem);
+
+    const notChangedElem = screen.queryByText("Not changed!");
+    expect(notChangedElem).toBeNull();
   });
 });
