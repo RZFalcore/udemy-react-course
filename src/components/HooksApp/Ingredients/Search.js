31db8796152eefec6d52c8ade61daccfc93f24cf
Eugene Roy <box.rz.box@gmail.com>
2022-12-07 18:03:52 +0200

    add filter request delay

--- src/components/HooksApp/Ingredients/Search.js
+++ src/components/HooksApp/Ingredients/Search.js
@@ -1,32 +1,38 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import Card from "../UI/Card";
 import "./Search.css";
 
 const Search = React.memo(({ onLoadIngridients }) => {
   const [filter, setFilter] = useState("");
+  const inputRef = useRef();
 
   useEffect(() => {
-    const querry =
-      filter.length === 0 ? "" : `?orderBy="title"&equalTo="${filter}"`;
+    const delay = setTimeout(() => {
+      if (filter === inputRef.current.value) {
+        const querry =
+          filter.length === 0 ? "" : `?orderBy="title"&equalTo="${filter}"`;
 
-    fetch(
-      "https://ud-react-http-default-rtdb.europe-west1.firebasedatabase.app/ingridients.json" +
-        querry
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        const ingridientsList = [];
-        for (const k in data) {
-          ingridientsList.push({
-            id: k,
-            title: data[k].title,
-            amount: data[k].amount,
-          });
-        }
-        onLoadIngridients(ingridientsList);
-      })
-      .catch((e) => console.log(e));
+        fetch(
+          "https://ud-react-http-default-rtdb.europe-west1.firebasedatabase.app/ingridients.json" +
+            querry
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            const ingridientsList = [];
+            for (const k in data) {
+              ingridientsList.push({
+                id: k,
+                title: data[k].title,
+                amount: data[k].amount,
+              });
+            }
+            onLoadIngridients(ingridientsList);
+          })
+          .catch((e) => console.log(e));
+      }
+    }, 300);
+    return () => clearTimeout(delay);
   }, [filter, onLoadIngridients]);
 
   const changeFilterHandler = (e) => setFilter(e.target.value);
@@ -35,7 +41,12 @@ const Search = React.memo(({ onLoadIngridients }) => {
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" value={filter} onChange={changeFilterHandler} />
+          <input
+            type="text"
+            ref={inputRef}
+            value={filter}
+            onChange={changeFilterHandler}
+          />
         </div>
       </Card>
     </section>
